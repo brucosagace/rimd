334226dfe88971033bafd5aca2acca0b7144589b
Per Stenström <per@vinnovera.se>
2013-11-19 12:42:28 +0100

    RessImage class

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-	# Install PSR-0-compatible class autoloader
-	spl_autoload_register(function($class){
-		require preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
-	});
+	include 'libs/autoloader.php';
 
 	# Get Rimd class
 	use \Pstenstrm\Markdown;
---
+++ libs/autoloader.php
@@ -0,0 +1,5 @@
+<?php 
+# Install PSR-0-compatible class autoloader
+spl_autoload_register(function($class){
+	require preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
+});
