cbe2af4d27c078ab89b55ed1ee76c7a44c97e5aa
Per Stenström <per@vinnovera.se>
2014-08-21 15:23:10 +0200

    Lazyloading and re-eveluation on resize

--- js/rimd.js
+++ js/rimd.js
@@ -1,108 +1,176 @@
+/*!
+ * 
+ */
+
 (function(){
+	var
+		win = window;
+		doc = document;
+
 	"use strict";
 
-	var Rimd = function(params){
-		var test, options = {};
+	var Rimd = function(params) {
+		var 
+			options = {},
+			defaults = {
+				className:      'rimd_img',
+				widths:         ['320', '600', '1024'],
+				heights:        ['320', '600', '1024'],
+				path:           'resimagecrop.php?image={path}&w={width}&h={height}&r={retina}',
+				reloadOnResize: false,
+				lazyload:       false
+			},
+			test;
 
-		init(params);
+		options = extend(defaults, params);
 
-		function init(params){
-			var defaults = {
-			    	className: 'rimd_img',
-			    	widths:    ['320', '600', '1024'],
-			    	path:      'resimagecrop.php?image={path}&w={width}&r={retina}'
-			    };
-
-			options = extend(defaults, params);
-
-			doImages();
-		}
+		doImages();
 
 		function doImages() {
-			var i,
-			    widths = {}, 
-			    images = getElementByClass(options.className);
+			var 
+				paths = [],
+				images = getElementByClass(options.className),
+				attr = getImageAttributes(images),
+				resizeHandler, i;
 
-			// Batch width checks to minimize the impact of forced reflow
 			for (i in images) {
 				if(images.hasOwnProperty(i) && i !== 'length') {
-					widths[i] = images[i].offsetWidth;
+					parseImage(images[i], attr[i]);
 				}
 			}
 
-			i = null;
+			if(options.reloadOnResize) {
+				resizeHandler = throttle(function () {
+					var
+						newAttr = getImageAttributes(images),
+						i;
 
-			for (i in images) {
-				if(images.hasOwnProperty(i) && i !== 'length') {
-					parseImage(images[i], widths[i]);
-				}
+					for (i in images) {
+						if(images.hasOwnProperty(i) && i !== 'length') {
+							if(attr[i].path !== getImagePath(newAttr[i])) {
+								parseImage(images[i], newAttr[i]);
+							}
+						}
+					}
+
+					attr = newAttr;
+				});
+
+				win.addEventListener('resize', resizeHandler);
 			}
 		}
 
-		function parseImage(image, width) {
-			var newImage, attr;
-			
-			attr = getImageAttributes(image);
+		
+
+		function parseImage(image, attr) {
+			var 
+				img = image.getElementsByTagName('img'),
+				newImage;
 
 			if(!attr.src || !attr.src[1]) return;
-			attr.offsetWidth = width;
+
+			if(img.length) {
+				img[0].parentNode.removeChild(img[0]);
+			}
 
 			newImage = createNewImage(attr);
 
-			image.appendChild(newImage);
+			if(!options.lazyload || isElementInViewport(image)) {
+				image.appendChild(newImage);
+			} else {
+				lazyloadImage(image, newImage);
+			}
+		}
+
+		function lazyloadImage (image, newImage) {
+			var
+				scrollHandler = throttle(function() {
+					if(isElementInViewport(image)) {
+						image.appendChild(newImage);
+
+						win.removeEventListener('scroll', scrollHandler);
+						win.removeEventListener('resize', resizeHandler);
+					}
+				}),
+				resizeHandler = throttle(function() {
+					image.dataset.top = image.getBoundingClientRect().top;
+					scrollHandler();
+				});
+
+			win.addEventListener('scroll', scrollHandler); 
+			win.addEventListener('resize', resizeHandler);
 		}
 
 		function createNewImage(attr) {
-			var img = document.createElement('img');
+			var 
+				img = doc.createElement('img');
 
-			img.src = options.path.replace(/\{width\}|\{path\}|\{retina\}/g, function(match, tag, cha){
-				return pathReplace(attr, match, tag, cha);
-			});
+			img.src = attr.path;
 
 			if(attr.alt) img.alt = attr.alt;
 			if(attr.title) img.title = attr.title;
 
 			return img;
-    }
+		}
 
-    function pathReplace(attr, match) {
+		function getImagePath(attr) {
+			return options.path.replace(/\{width\}|\{path\}|\{retina\}|\{height\}/g, function(match, tag, cha){
+				return pathReplace(attr, match, tag, cha);
+			});
+		}
+
+		function pathReplace(attr, match) {
+			var
+				devicePixelRatio = win.devicePixelRatio,
+				multiplier = (devicePixelRatio > 1) ? 2 : 1;
+
 			switch (match) {
 				case '{path}':
 					return attr.src;
 				case '{width}':
-					return getClosestValues(options.widths, attr.offsetWidth);
+					return getClosestValues(options.widths, attr.offsetWidth) * multiplier;
+				case '{height}':
+					return getClosestValues(options.heights, attr.offsetHeight) * multiplier;
 				case '{retina}':
-					return (window.devicePixelRatio > 1) ? 1 : 0;
+					return (devicePixelRatio > 1) ? 1 : 0;
 			}
 		}
 
-		function getImageAttributes(image) {
-			var noscript = image.children[0],
-			    content  = noscript.textContent || noscript.innerHTML,
-			    srcRex   = /src="([^"]+)/g,
-			    altRex   = /alt="([^"]+)/g,
-			    titleRex = /title="([^"]+)/g,
-			    result   = {},
-			    src, alt, title;
+		function getImageAttributes(images) {
+			var 
+				attr = {},
+				data, key, i;
 
-			src = srcRex.exec(content);
-			alt = altRex.exec(content);
-			title = titleRex.exec(content);
+			for (i in images) {
+				if(images.hasOwnProperty(i) && i !== 'length') {
+					attr[i] = {};
 
-			if(src && src[1]) result.src = src[1];
-			if(alt && alt[1]) result.alt = alt[1];
-			if(title && title[1]) result.title = title[1];
+					attr[i].offsetWidth = images[i].offsetWidth;
+					attr[i].offsetHeight = images[i].offsetHeight;
+					
+					data = images[i].children[0].dataset;
 
-			return result;
+					for(key in data) {
+						if(data.hasOwnProperty(key)) {
+							attr[i][key] = data[key];
+						}
+					}
+
+					attr[i].path = getImagePath(attr[i]);
+				}
+			}
+
+			return attr;
 		}
 
 		function getElementByClass(selector) {
-			var result = [];
+			var 
+				result = [];
 
 			selector = selector.replace(/[.]/, '');
 
-			if(document.querySelectorAll) {
-				result = document.querySelectorAll('.' + selector);
+			if(doc.querySelectorAll) {
+				result = doc.querySelectorAll('.' + selector);
 			} else {
 				result = legacyGetElementByClass(selector);
 			}
@@ -111,9 +179,10 @@
     }
 
     function legacyGetElementByClass(selector) {
-			var result = [],
-					elems = document.getElementsByTagName('*'),
-					i;
+			var 
+				result = [],
+				elems = doc.getElementsByTagName('*'),
+				i;
 
 			for (i in elems) {
 				if((' ' + elems[i].className + ' ').indexOf(' ' + selector + ' ') > -1) {
@@ -132,10 +201,41 @@
 			return destination;
 		}
 
-		function getClosestValues (stack, needle) {
-			var lowDiff, diff, result,
-			    i   = 0,
-			    len = stack.length;
+		function throttle(fn, threshhold, context) {
+			var 
+				last, deferTimer;
+
+			threshhold || (threshhold = 17); // ~ 1000 / 60
+
+			return function() {
+				var
+					now = +new Date,
+					args = arguments;
+
+				context || (context = this);
+
+				if (last && now < last + threshhold) {
+					// hold on to it
+					clearTimeout(deferTimer);
+					deferTimer = setTimeout(function() {
+						last = now;
+						fn.apply(context, args);
+					}, threshhold);
+				} else {
+					last = now;
+					fn.apply(context, args);
+				}
+			};
+		}
+
+		function getClosestValues(stack, needle) {
+			var
+				i = 0,
+				len, lowDiff, diff, result;
+
+			if(!stack) return needle;
+
+			len = stack.length;
 
 			for (; i < len; i++) {
 				diff = stack[i] - needle;
@@ -148,6 +248,19 @@
 			}
 
 			return result;
+		}
+
+		function isElementInViewport(el) {
+			var 
+				top = el.dataset.top || el.getBoundingClientRect().top,
+				docEl = doc.documentElement,
+				isInViewport = top <= (win.pageYOffset || docEl.scrollTop)  - (docEl.clientTop || 0) + (win.innerHeight || docEl.clientHeight);
+
+			if(!isInViewport && !el.dataset.top) {
+				el.dataset.top = top;
+			}
+
+			return isInViewport;
 		}
 
 		// UglifyJS will discard any code within an if (DEBUG) clause
@@ -167,5 +280,5 @@
 		};
 	};
 
-	window.Rimd = Rimd;
+	win.Rimd = Rimd;
 })();
